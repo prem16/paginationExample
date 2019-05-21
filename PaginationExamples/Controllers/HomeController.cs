9312c7da32a5d1a4fc63d0f82b4bfc1098f04ce4
prem12 <murmu.1993@gmail.com>
2019-05-22 03:14:47 +0530

    Second commit for pagination

---
+++ PaginationExamples/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using PaginationExamples.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaginationExamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index(int? page)
+        {
+            var dummyItems = Enumerable.Range(1, 150).Select(x => "Item " + x);
+            var pager = new Pager(dummyItems.Count(), page);
+
+            var viewModel = new IndexViewModel
+            {
+                Items = dummyItems.Skip((pager.CurrentPage - 1) * pager.PageSize).Take(pager.PageSize),
+                Pager = pager
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ PaginationExamples/Models/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaginationExamples.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<string> Items { get; set; }
+        public Pager Pager { get; set; }
+    }
+}
